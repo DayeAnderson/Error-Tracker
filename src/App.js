10c26fe740c7a7ea00239ff2143245d6943e2b62
David Anderson <djathree92@gmail.com>
2021-07-11 17:08:35 -0400

    add dashboard page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {useSelector} from 'react-redux'
 import Login from './Views/Pages/Login/login'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import Sidebar from './Views/Sidebar/sidebar'
+import Dashboard from './Views/Pages/Dashboard/dashboard'
 import ViewBugPage from './Views/Pages/viewBugs'
 import CreateBug from './Views/Components/BugCreate & Edit/bugForm'
 
@@ -14,6 +15,7 @@ function App() {
         <>
         <Sidebar />
         <Switch>
+          <Route path="/" exact><Dashboard /></Route>
           <Route path="/viewbugs"><ViewBugPage /></Route>
           <Router path="/create"><div className='page-container'><CreateBug title="Create Bug"/></div></Router>
         </Switch>
