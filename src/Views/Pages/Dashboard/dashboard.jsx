d75938d195efe8968a48c3830cfe781dd6c9e76c
David Anderson <djathree92@gmail.com>
2021-07-11 17:19:38 -0400

    add history to bug cards

--- src/Views/Pages/Dashboard/dashboard.jsx
+++ src/Views/Pages/Dashboard/dashboard.jsx
@@ -1,11 +1,13 @@
 import React,{useEffect} from 'react'
 import {useDispatch,useSelector} from 'react-redux'
+import {useHistory} from 'react-router-dom'
 import {getBugs} from '../../../Controllers/Redux/bugSlice'
 import Card from '../../Components/Dashboard/card'
 
 export default ()=>{
     const dispatch = useDispatch();
     const bugs = useSelector(state=>state.bugs);
+    const browserHistory = useHistory();
     let highCount = 0
     let midCount = 0
     let lowCount = 0
@@ -13,6 +15,9 @@ export default ()=>{
         highCount = filterBugs(1);
         midCount = filterBugs(2);
         lowCount = filterBugs(3);
+    }
+    function redirect() {
+        browserHistory.push('/viewbugs')
     }
 
     function filterBugs(priority){
@@ -25,9 +30,9 @@ export default ()=>{
 
     return(
         <div className='page-container'>
-            <Card priority="1" count={highCount.length} />
-            <Card priority="2" count={midCount.length} />
-            <Card priority="3" count={lowCount.length} />
+            <Card priority="1" count={highCount.length} clicked={redirect} />
+            <Card priority="2" count={midCount.length} clicked={redirect} />
+            <Card priority="3" count={lowCount.length} clicked={redirect} />
 
         </div>
     )
